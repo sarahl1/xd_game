653446dd1d7d52aef907bb5bcba2f3baca0dc3c5
jimmy-gnahz <jimmyjimbozhang@gmail.com>
2019-02-10 10:43:55 -0800

    Separated out environment creation code from game.js

--- js/game.js
+++ js/game.js
@@ -1,9 +1,12 @@
+import StaticEnvironment from "./staticEnvironment.js";
+
 const width = 800;
 const height = 600;
 const xvel = 300;
 const yvel = 300;
 var max = 0;
 var items = Array(1, 2, 3, 4, 5, 6, 7);
+
 
 document.onreadystatechange = function () {
     if (document.readyState == "complete") {
@@ -14,67 +17,15 @@ document.onreadystatechange = function () {
             height: height
         });
 
-
         var backgroundLayer = game.createLayer('background');
-        var grass = backgroundLayer.createEntity();
         backgroundLayer.static = true;
-        grass.pos = { x: 0, y: 0 };
-        grass.asset = new PixelJS.Tile();
-        grass.asset.prepare({
-            name: 'grass.png',
-            size: {
-                width: width,
-                height: height
-            }
-        });
 
-        var rightWall = backgroundLayer.createEntity();
-        rightWall.pos = { x: width, y: 0 };
-        rightWall.size = { width: 10, height: height };
-        rightWall.asset = new PixelJS.AnimatedSprite();
-        rightWall.asset.prepare({
-            name: 'garbagefire.png',
-            frames: 1,
-            rows: 1,
-            speed: 80,
-            defaultFrame: 0
-        });
+        var staticEnvironment = new StaticEnvironment(backgroundLayer);
+        var rightWall, leftWall, topWall, botWall;
+        var grass;
 
-        var leftWall = backgroundLayer.createEntity();
-        leftWall.pos = { x: -420, y: 0 };
-        leftWall.size = { width: 420, height: height };
-        leftWall.asset = new PixelJS.AnimatedSprite();
-        leftWall.asset.prepare({
-            name: 'garbagefire.png',
-            frames: 1,
-            rows: 1,
-            speed: 80,
-            defaultFrame: 0
-        });
-
-        var topWall = backgroundLayer.createEntity();
-        topWall.pos = { x: 0, y: -430 };
-        topWall.size = { width: width, height: 430 };
-        topWall.asset = new PixelJS.AnimatedSprite();
-        topWall.asset.prepare({
-            name: 'garbagefire.png',
-            frames: 1,
-            rows: 1,
-            speed: 80,
-            defaultFrame: 0
-        });
-
-        var botWall = backgroundLayer.createEntity();
-        botWall.pos = { x: 0, y: height };
-        botWall.size = { width: width, height: 10 };
-        botWall.asset = new PixelJS.AnimatedSprite();
-        botWall.asset.prepare({
-            name: 'garbagefire.png',
-            frames: 1,
-            rows: 1,
-            speed: 80,
-            defaultFrame: 0
-        });
+        [rightWall,leftWall, topWall, botWall] = staticEnvironment.createWalls();
+        grass = staticEnvironment.createGrass();
 
         var playerLayer = game.createLayer("players");
         var player = new PixelJS.Player();
