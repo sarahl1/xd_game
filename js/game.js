8a4b23935a676fb354fdf4211dec4679d2905d0b
Sarah <sarahli99@hotmail.com>
2019-02-09 23:19:36 -0800

    boundaries set up

--- js/game.js
+++ js/game.js
@@ -1,10 +1,15 @@
+const width = 800;
+const height = 600;
+const xvel = 200;
+const yvel = 200;
+
 document.onreadystatechange = function () {
     if (document.readyState == "complete") {
         var game = new PixelJS.Engine();
         game.init({
             container: 'game_container',
-            width: 800,
-            height: 600
+            width: width,
+            height: height
         });
 
 
@@ -16,9 +21,57 @@ document.onreadystatechange = function () {
         grass.asset.prepare({
             name: 'grass.png',
             size: {
-                width: 800,
-                height: 600
+                width: width,
+                height: height
             }
+        });
+
+        var rightWall = backgroundLayer.createEntity();
+        rightWall.pos = { x: width, y: 0 };
+        rightWall.size = { width: 10, height: height };
+        rightWall.asset = new PixelJS.AnimatedSprite();
+        rightWall.asset.prepare({
+            name: 'garbagefire.png',
+            frames: 1,
+            rows: 1,
+            speed: 80,
+            defaultFrame: 0
+        });
+
+        var leftWall = backgroundLayer.createEntity();
+        leftWall.pos = { x: -420, y: 0 };
+        leftWall.size = {width: 420, height: height};
+        leftWall.asset = new PixelJS.AnimatedSprite();
+        leftWall.asset.prepare({
+            name: 'garbagefire.png',
+            frames: 1,
+            rows: 1,
+            speed: 80,
+            defaultFrame: 0
+        });
+
+        var topWall = backgroundLayer.createEntity();
+        topWall.pos = { x: 0, y: -430 };
+        topWall.size = {width: width, height: 430};
+        topWall.asset = new PixelJS.AnimatedSprite();
+        topWall.asset.prepare({
+            name: 'garbagefire.png',
+            frames: 1,
+            rows: 1,
+            speed: 80,
+            defaultFrame: 0
+        });
+
+        var botWall = backgroundLayer.createEntity();
+        botWall.pos = { x: 0, y: height };
+        botWall.size = {width: width, height: 10};
+        botWall.asset = new PixelJS.AnimatedSprite();
+        botWall.asset.prepare({
+            name: 'garbagefire.png',
+            frames: 1,
+            rows: 1,
+            speed: 80,
+            defaultFrame: 0
         });
 
         var playerLayer = game.createLayer("players");
@@ -26,7 +79,7 @@ document.onreadystatechange = function () {
         player.addToLayer(playerLayer);
         player.pos = { x: 250, y: 300 };
         player.size = { width: 64, height: 64 };
-        player.velocity = { x: 200, y: 200 };
+        player.velocity = { x: xvel, y: yvel };
         player.asset = new PixelJS.AnimatedSprite();
         player.asset.prepare({
             name: 'char.png',
@@ -35,6 +88,7 @@ document.onreadystatechange = function () {
             speed: 50,
             defaultFrame: 1
         });
+
 
         var itemLayer = game.createLayer('items');
         var coin = itemLayer.createEntity();
@@ -49,10 +103,22 @@ document.onreadystatechange = function () {
             defaultFrame: 0
         });
 
+        var compost = itemLayer.createEntity();
+        compost.pos = { x: 420, y: 170 };
+        compost.size = { width: 16, height: 16 };
+        compost.asset = new PixelJS.AnimatedSprite();
+        compost.asset.prepare({
+            name: 'compost.png',
+            frames: 1,
+            rows: 1,
+            speed: 80,
+            defaultFrame: 0
+        });
+
         var itemLayer1 = game.createLayer('items1');
         var fire = itemLayer1.createEntity();
-        fire.pos = { x: 200, y: 100};
-        fire.size = {width: 16, height: 16};
+        fire.pos = { x: 200, y: 100 };
+        fire.size = { width: 16, height: 16 };
         fire.asset = new PixelJS.AnimatedSprite();
         fire.asset.prepare({
             name: 'garbagefire.png',
@@ -83,12 +149,32 @@ document.onreadystatechange = function () {
                     '#FFFFFF',
                     'left'
                 );
+            } if (entity === compost) {
+                player.pos = { x: 0, y: 0 };
+                setTimeout(function () {
+                    player.velocity = { x: xvel, y: yvel };
+                }, 500);
+            } if (entity === rightWall){
+                player.pos.x = rightWall.pos.x-68;
+            } if (entity === leftWall){
+                player.pos.x = leftWall.pos.x+425;
+            } if (entity === topWall){
+                player.pos.y = topWall.pos.y+435;
+            } if (entity === botWall){
+                player.pos.y = botWall.pos.y-68;
             }
 
         });
 
+        backgroundLayer.registerCollidable(rightWall);
+        backgroundLayer.registerCollidable(leftWall);
+        backgroundLayer.registerCollidable(topWall);
+        backgroundLayer.registerCollidable(botWall);
+        
         playerLayer.registerCollidable(player);
         itemLayer.registerCollidable(coin);
+        itemLayer.registerCollidable(compost);
+
 
 
 
@@ -101,16 +187,16 @@ document.onreadystatechange = function () {
         holdingLayer.static = true;
 
 
-        function updateTime(){
-                playerLayer.visible = false;
-                itemLayer.visible = false;
-                itemLayer1.visible = true;
+        function updateTime() {
+            playerLayer.visible = false;
+            itemLayer.visible = false;
+            itemLayer1.visible = true;
         }
 
         game.loadAndRun(function (elapsedTime, dt) {
-            setTimeout(function(){
+            setTimeout(function () {
                 updateTime();
-            }, 2000);
+            }, 60000);
 
         });
     }
